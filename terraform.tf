757e42502090a8d0add95646f72a6cef1a9ee1ef
Pratik Khasnabis <pratik.khasnabis@outlook.com>
2022-09-23 19:21:27 +1000

    removed compute resources

--- terraform.tf
+++ terraform.tf
@@ -5,19 +5,7 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.23"
     }
-
-    tls = {
-      source  = "hashicorp/tls"
-      version = "~>4.0"
-    }
   }
-
-  # backend "azurerm" {
-  #   resource_group_name  = "pratik-terraform"
-  #   storage_account_name = "pktfstatecontainer"
-  #   container_name       = "largestate"
-  #   key                  = "dev.tfstate"
-  # }
 
   cloud {
     organization = "pratik-hc"
